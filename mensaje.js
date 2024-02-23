85a6b869219b384ae4ed55a864bf9ec3ed41b9c4
montalvo1414 <140976611+montalvo1414@users.noreply.github.com>
2024-02-23 14:04:24 -0500

    Update mensaje.js

--- mensaje.js
+++ mensaje.js
@@ -2,23 +2,36 @@ document.addEventListener("DOMContentLoaded", function() {
   // Ocultar el contenido del diario al cargar la página
   var elementosDiario = document.querySelectorAll('.diario .di');
   for (var i = 0; i < elementosDiario.length; i++) {
-      elementosDiario[i].style.display = 'none';
+    elementosDiario[i].style.display = 'none';
   }
 });
+
 function mostrar(id) {
   var elementoMostrar = document.getElementById(id);
-  // Verificar si el elemento está visible
   if (elementoMostrar.style.display === 'block') {
-    // Si está visible, se oculta
     elementoMostrar.style.display = 'none';
   } else {
-    // Si no está visible, se muestra y se ocultan los demás
-    var elementosDiario = document.querySelectorAll('.diario p');
+    // Ocultar todos los elementos del diario antes de mostrar el deseado
+    var elementosDiario = document.querySelectorAll('.diario .di');
     for (var i = 0; i < elementosDiario.length; i++) {
       elementosDiario[i].style.display = 'none';
     }
-    elementoMostrar.style.display = 'block';
+    elementoMostrar.style.display = 'block'; 
   }
-}
 
+  // Verificar si estamos en el viewport definido en @media
+  var viewportWidth = window.innerWidth || document.documentElement.clientWidth;
+  if (viewportWidth <= 500) {
+    var dias = document.querySelector('.dias');
+    dias.style.display = 'none';
+  }
+  // Agregar evento clic a los elementos con la clase 'lavel' para mostrar el elemento 'dias'
+var lavel = document.querySelectorAll('.lavel');
+for (var i = 0; i < lavel.length; i++) {
+  lavel[i].addEventListener('click', function() {
+    var dias = document.querySelector('.dias');
+    dias.style.display = 'block';
+  });
+}
+}
 
