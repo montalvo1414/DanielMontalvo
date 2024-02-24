31c946da1d890f3b878d5125c58c0d5b0b2c5c76
montalvo1414 <140976611+montalvo1414@users.noreply.github.com>
2024-02-23 21:34:03 -0500

    Update invitacion2.js

--- invitacion2.js
+++ invitacion2.js
@@ -2,6 +2,6 @@ let boton_no = document.getElementsByClassName('move')[0];
     
     boton_no.onmouseover = () => {
           boton_no.style.position = "absolute";
-          boton_no.style.left = Math.random()*700 + 'px';
-          boton_no.style.top = Math.random()*500 + 'px';
+          boton_no.style.left = Math.random()*70 + '%';
+          boton_no.style.top = Math.random()*50 + '%';
     }
