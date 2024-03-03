75f17d5cdb612a6c568ed20c90fed29d7ab93d70
montalvo1414 <140976611+montalvo1414@users.noreply.github.com>
2024-03-02 21:28:54 -0500

    Add files via upload

--- mensaje.js
+++ mensaje.js
@@ -4,6 +4,15 @@ function selectContact(contact) {
     selectedContact = contact;
     document.getElementById("chat-container").style.display = "block";
     document.getElementById("mensajes").innerHTML = ""; // Clear previous messages
+    
+    var width = window.innerWidth;
+    var contacts = document.getElementById("contactos");
+    var chatContainer = document.getElementById("chat-container");
+
+    if (width <= 500) { // Verificar el ancho de la ventana
+        contacts.style.display = "none"; // Ocultar la lista de contactos solo si el ancho es menor o igual a 500px
+        chatContainer.style.display = "block"; // Mostrar el chat-container
+    }
 }
 
 function sendMessage() {
@@ -23,17 +32,32 @@ function sendMessage() {
 
 function hideContacts() {
     var width = window.innerWidth;
-    if (width <= 600) { // Verificar si el ancho de la ventana es menor o igual a 600px
-        var contacts = document.getElementById("contactos");
+    var contacts = document.getElementById("contactos");
+    var chatContainer = document.getElementById("chat-container");
+
+    if (width <= 500) { // Si el ancho es menor o igual a 600px, mostrar los contactos y ocultar el chat
+        contacts.style.display = "block";
+        chatContainer.style.display = "none";
+    } else if (width <= 500 && selectedContact !== null) { // Si el ancho es menor o igual a 500px y hay un contacto seleccionado, ocultar los contactos
         contacts.style.display = "none";
+    } else { // Si el ancho es mayor a 600px o no hay contacto seleccionado, mostrar tanto los contactos como el chat
+        contacts.style.display = "block";
+        chatContainer.style.display = "block";
     }
 }
+// Asegúrate de llamar a hideContacts al cargar la página y en cualquier evento de redimensionamiento de la ventana
+window.onload = hideContacts; // Llamar a hideContacts al cargar la página
+window.onresize = hideContacts;
 
 function toggleContacts() {
     var contacts = document.getElementById("contactos");
-    if (contacts.style.display === "none" || contacts.style.display === "") {
+    var chatContainer = document.getElementById("chat-container");
+    
+    if (contacts.style.display === "none" || contacts.style.display === "none") {
         contacts.style.display = "block";
+        chatContainer.style.display = "none"
     } else {
-        contacts.style.display = "none";
+        contacts.style.display = "block";
+        chatContainer.style.display = "none"
     }
 }
