d6e189cd17196462d7835d5f75dcbd4481031c87
montalvo1414 <140976611+montalvo1414@users.noreply.github.com>
2024-03-04 15:03:52 -0500

    Update mensaje.js

--- mensaje.js
+++ mensaje.js
@@ -55,7 +55,7 @@ function toggleContacts() {
     
     if (contacts.style.display === "none" || contacts.style.display === "none") {
         contacts.style.display = "block";
-        chatContainer.style.display = "none"
+        chatContainer.style.display = "block"
     } else {
         contacts.style.display = "block";
         chatContainer.style.display = "none"
