463a90e5353a3cf748f1b1e52a52809d6e05ab0d
montalvo1414 <140976611+montalvo1414@users.noreply.github.com>
2024-04-05 18:17:26 -0500

    Delete invitacion2.js

--- invitacion2.js
+++
@@ -1,7 +0,0 @@
-let boton_no = document.getElementsByClassName('move')[0];
-    
-    boton_no.onmouseover = () => {
-          boton_no.style.position = "absolute";
-          boton_no.style.left = Math.random()*70 + '%';
-          boton_no.style.top = Math.random()*40 + '%';
-    }
